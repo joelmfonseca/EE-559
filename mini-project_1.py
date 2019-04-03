693231f23dde1a528a53d21c5c1cd9fe37fb2769
Pierre Armel <pierre.mbangandjock@epfl.ch>
2019-04-03 16:58:07 +0200

    defines model using weight sharing

--- mini-project_1.py
+++ mini-project_1.py
@@ -11,7 +11,10 @@ from torch.autograd import Variable
 from torch.nn import functional as F
 from utils import *
 
-class Net1(nn.Module):
+class MergeImages(nn.Module):
+    '''
+    
+    '''
     def __init__(self, nb_hidden=100):
         super(Net1, self).__init__()
         self.conv1 = nn.Conv2d(2, 32, kernel_size=3)
@@ -87,7 +90,41 @@ class Net3(nn.Module):
         pred = torch.tensor([compare_pair(i) for i in x])
         
         return pred
-    
+
+class WeightSharing(nn.Module):
+    def __init__(self, nb_hidden=100):
+        super(WeightSharing, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.fc1 = nn.Linear(512, nb_hidden)
+        self.fc2 = nn.Linear(nb_hidden, 1)
+
+        self.criterion = nn.BCEWithLogitsLoss()
+        self.target_type = torch.FloatTensor
+
+
+    def forward(self, x):
+        x1 = x[:, 0, :, :].view(-1, 1, x.shape[2], x.shape[3]) 
+        x1 = F.relu(F.max_pool2d(self.conv1(x1), kernel_size=2, stride=2))
+        x1 = F.relu(F.max_pool2d(self.conv2(x1), kernel_size=2, stride=2))
+        x1 = x1.view(-1, x1.shape[1]*x1.shape[2]*x1.shape[3])
+
+        x2 = x[:, 1, :, :].view(-1, 1, x.shape[2], x.shape[3])
+        x2 = F.relu(F.max_pool2d(self.conv1(x2), kernel_size=2, stride=2))
+        x2 = F.relu(F.max_pool2d(self.conv2(x2), kernel_size=2, stride=2))
+        x2 = x2.view(-1, x2.shape[1]*x2.shape[2]*x2.shape[3])
+
+        x = torch.cat((x1, x2), 1)
+        # print(x1.shape, x2.shape, x.shape)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        return x.view(-1)
+
+
+    def predict(self, x):
+        return torch.sigmoid(self.forward(x)).round()
+
 def train_model(model, optimizer, nb_epochs, train_input, train_target ,mini_batch_size):
 
     start = time.time()
@@ -145,8 +182,10 @@ if __name__ == '__main__':
     NB_EPOCHS = 25
     MINI_BATCH_SIZE = 100
     models = [Net1, Net2, Net3]
+    models = [WeightSharing]
     optimizers = [optim.SGD, optim.Adam]
     learning_rates = [1e-1, 1e-2, 1e-3]
+
 
     for m in models:
 
