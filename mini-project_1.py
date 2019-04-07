c1295dce110c21d82148da3bc4d6661feb431112
joelmfonseca <fonsecamateus.joel@gmail.com>
2019-04-07 23:40:56 +0200

    tested other configurations for auxiliary loss

--- mini-project_1.py
+++ mini-project_1.py
@@ -202,6 +202,52 @@ class NetAuxiliary1(nn.Module):
 
     def predict(self, x):
         return torch.max(self.forward(x)[1], 1)[1]
+
+class NetAuxiliary2(nn.Module):
+    def __init__(self, nb_hidden=100):
+        super(NetAuxiliary2, self).__init__()
+        self.conv1 = nn.Conv2d(2, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.fc1 = nn.Linear(256, nb_hidden)
+        self.fc2 = nn.Linear(nb_hidden, 10)
+        self.fc3 = nn.Linear(nb_hidden, 2)
+
+        self.criterion = nn.CrossEntropyLoss()
+        self.target_type = torch.LongTensor
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=2, stride=2))
+        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2, stride=2))
+        x = F.relu(self.fc1(x.view(-1, 256)))
+        z = self.fc2(x)
+        y = self.fc3(x)
+        return z, y
+
+    def predict(self, x):
+        return torch.max(self.forward(x)[1], 1)[1]
+
+class NetAuxiliary3(nn.Module):
+    def __init__(self, nb_hidden=100):
+        super(NetAuxiliary3, self).__init__()
+        self.conv1 = nn.Conv2d(2, 32, kernel_size=3)
+        self.fc1 = nn.Sequential(nn.Linear(1152, 500), nn.Linear(500, 100), nn.Linear(100, 10))
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.fc2 = nn.Linear(256, nb_hidden)
+        self.fc3 = nn.Linear(nb_hidden, 2)
+
+        self.criterion = nn.CrossEntropyLoss()
+        self.target_type = torch.LongTensor
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=2, stride=2))
+        z = self.fc1(x.view(-1, 1152))
+        x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=2, stride=2))
+        x = F.relu(self.fc2(x.view(-1, 256)))
+        y = self.fc3(x)
+        return z, y
+
+    def predict(self, x):
+        return torch.max(self.forward(x)[1], 1)[1]
     
 def train_model_auxiliary(model, optimizer, nb_epochs, train_input, train_target_bin,
                             train_target_class, mini_batch_size):
@@ -217,7 +263,8 @@ def train_model_auxiliary(model, optimizer, nb_epochs, train_input, train_target
             loss_bin = model.criterion(output_bin, target_bin)
             loss_class = model.criterion(output_class, target_class)
 
-            loss = loss_bin + 0.1*loss_class
+            lambda_ = 0
+            loss = loss_bin + lambda_*loss_class
 
             model.zero_grad()
             loss.backward()
@@ -285,7 +332,7 @@ if __name__ == '__main__':
     NB_EPOCHS = 25
     MINI_BATCH_SIZE = 100
     # models = [Net1, Net2, Net3, NetSharing1, NetSharing2]
-    models = [NetAuxiliary1]
+    models = [NetAuxiliary3]
     optimizers = [optim.SGD, optim.Adam]
     learning_rates = [1e-1, 1e-2, 1e-3]
 
